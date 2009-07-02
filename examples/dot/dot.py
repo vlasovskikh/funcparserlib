6639dc909a1bfce237ba0fe89541af81a3b8fdb9
Andrey Vlasovskikh <vlasovskikh@aivt.ftk.spbstu.ru>
2009-07-02 19:28:08 +0400

    examples/dot: Fixed Python < 2.6 issues

--- examples/dot/dot.py
+++ examples/dot/dot.py
@@ -6,8 +6,29 @@ from pprint import pformat
 from funcparserlib.lexer import make_tokenizer, Token, LexerError
 from funcparserlib.parser import (some, a, maybe, many, finished, skip,
     oneplus, NoParseError)
+try:
+    from collections import namedtuple
+except ImportError:
+    # Примитивная реализация namedtuple для 2.1 < Python < 2.6
+    def namedtuple(name, fields):
+        'Only space-delimited fields are supported.'
+        def prop(i, name):
+            return (name, property(lambda self: self[i]))
+        methods = dict(prop(i, f) for i, f in enumerate(fields.split(' ')))
+        methods.update({
+            '__new__': lambda cls, *args: tuple.__new__(cls, args),
+            '__repr__': lambda self: '%s(%s)' % (
+                name,
+                ', '.join('%s=%r' % (
+                    f, getattr(self, f)) for f in fields.split(' ')))})
+        return type(name, (tuple,), methods)
 
 ENCODING = 'utf-8'
+
+Graph = namedtuple('Graph', 'strict type id stmts')
+Node = namedtuple('Node', 'id attrs')
+Attr = namedtuple('Attr', 'name value')
+Edge = namedtuple('Edge', 'id links attrs')
 
 def tokenize(str):
     'str -> Sequence(Token)'
@@ -26,14 +47,7 @@ def tokenize(str):
 
 def parse(seq):
     'Sequence(Token) -> object'
-    # FIXME: Python >= 2.6 only
-    from collections import namedtuple
     unarg = lambda f: lambda args: f(*args)
-    Graph = unarg(namedtuple('Graph', 'strict type id stmts'))
-    Node = unarg(namedtuple('Node', 'id attrs'))
-    Attr = unarg(namedtuple('Attr', 'name value'))
-    Edge = unarg(namedtuple('Edge', 'id links attrs'))
-
     tokval = lambda x: x.value
     flatten = lambda list: sum(list, [])
     n = lambda s: a(Token('Name', s)) >> tokval
@@ -45,13 +59,17 @@ def parse(seq):
         id +
         maybe(skip(op('=')) + id) +
         skip(maybe(op(',')))
-        >> Attr)
+        >> unarg(Attr))
     attr_list = (
         many(skip(op('[')) + many(a_list) + skip(op(']')))
         >> flatten).named('attr_list')
-    node_stmt = (node_id + attr_list >> Node).named('node_stmt')
+    node_stmt = (node_id + attr_list >> unarg(Node)).named('node_stmt')
     edge_rhs = skip(op('->') | op('--')) + node_id
-    edge_stmt = (node_id + oneplus(edge_rhs) + attr_list >> Edge).named('edge_stmt')
+    edge_stmt = (
+        node_id +
+        oneplus(edge_rhs) +
+        attr_list
+        >> unarg(Edge)).named('edge_stmt')
     stmt = (
         edge_stmt
         | node_stmt
@@ -67,7 +85,7 @@ def parse(seq):
         skip(op('{')) +
         stmt_list +
         skip(op('}'))
-        >> Graph).named('graph')
+        >> unarg(Graph)).named('graph')
     dotfile = graph + skip(finished)
 
     return dotfile.parse(seq)

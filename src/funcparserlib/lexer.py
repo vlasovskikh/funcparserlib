942519bb45bb674f2f3130da6db1e189c9786ce1
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2009-09-10 01:56:38 +0400

    Fixed #5: Updated __all__ field in funcpaserlib.lexer

--- src/funcparserlib/lexer.py
+++ src/funcparserlib/lexer.py
@@ -21,7 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-__all__ = ['tokenize', 'Token', 'LexerError']
+__all__ = ['make_tokenizer', 'Token', 'LexerError']
 
 import re
 

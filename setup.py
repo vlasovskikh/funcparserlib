5ff79c779940d5ee90e10cd22e41a6c20bded268
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2009-10-11 10:29:26 +0400

    m

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     package_dir={'': 'src'},
     author='Andrey Vlasovskikh',
     author_email='andrey.vlasovskikh@gmail.com',
-    description='Recursive descent parser library based on functional '
+    description='Recursive descent parsing library based on functional '
         'combinators',
     license='MIT',
     url='http://code.google.com/p/funcparserlib/')

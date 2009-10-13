177157adb0ba97a7337c1e5b7b48a75b935289d1
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2009-10-13 20:37:31 +0400

    Changed version to 0.4dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='funcparserlib',
-    version='0.3.4',
+    version='0.4dev',
     packages=['funcparserlib', 'funcparserlib.contrib'],
     package_dir={'': 'src'},
     author='Andrey Vlasovskikh',

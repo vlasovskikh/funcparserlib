1e1768cc2335537bef5d14eff89dca9ed4f5701a
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2011-05-29 19:41:45 +0400

    Converted JSON example tests to nosetests

---
+++ examples/json/test_json.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import eq_, raises
+from funcparserlib.parser import SyntaxError
+import json
+
+def t(data, expected=None):
+    eq_(json.loads(data), expected)
+
+def test_1_array():
+    t(u'[1]', [1])
+
+def test_1_object():
+    t(u'{"foo": "bar"}', {u'foo': u'bar'})
+
+def test_bool_and_null():
+    t(u'[null, true, false]', [None, True, False])
+
+def test_empty_array():
+    t(u'[]', [])
+
+def test_empty_object():
+    t(u'{}', {})
+
+def test_many_array():
+    t(u'[1, 2, [3, 4, 5], 6]', [1, 2, [3, 4, 5], 6])
+
+def test_many_object():
+    t(u'''
+        {
+            "foo": 1,
+            "bar":
+            {
+                "baz": 2,
+                "quux": [true, false],
+                "{}": {}
+            },
+            "spam": "eggs"
+        }
+    ''', {
+        u'foo': 1,
+        u'bar': {
+            u'baz': 2,
+            u'quux': [True, False],
+            u'{}': {},
+        },
+        u'spam': u'eggs',
+    })
+
+@raises(SyntaxError)
+def test_null():
+    t(u'')
+
+def test_numbers():
+    t(u'''\
+        [
+            0, 1, -1, 14, -14, 65536,
+            0.0, 3.14, -3.14, -123.456,
+            6.67428e-11, -1.602176e-19, 6.67428E-11
+        ]
+    ''', [
+        0, 1, -1, 14, -14, 65536,
+        0.0, 3.14, -3.14, -123.456,
+        6.67428e-11, -1.602176e-19, 6.67428E-11,
+    ])
+
+def test_strings():
+    t(r'''
+        [
+            ["", "hello", "hello world!"],
+            ["привет, мир!", "λx.x"],
+            ["\"", "\\", "\/", "\b", "\f", "\n", "\r", "\t"],
+            ["\u0000", "\u03bb", "\uffff", "\uFFFF"],
+            ["вот функция идентичности:\nλx.x\nили так:\n\u03bbx.x"]
+        ]
+    '''.decode('utf-8'), [
+        [u'', u'hello', u'hello world!'],
+        [u'привет, мир!', u'λx.x'],
+        [u'"', u'\\', u'/', u'\x08', u'\x0c', u'\n', u'\r', u'\t'],
+        [u'\u0000', u'\u03bb', u'\uffff', u'\uffff'],
+        [u'вот функция идентичности:\nλx.x\nили так:\n\u03bbx.x'],
+    ])
+
+@raises(SyntaxError)
+def test_toplevel_string():
+    t(u'неправильно')
+

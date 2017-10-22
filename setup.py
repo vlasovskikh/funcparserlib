ee193f86079682f7d3c4af81ab3cf6a9890f591d
Jon Dufresne <jon.dufresne@gmail.com>
2017-10-22 12:26:37 -0700

    Test all modern Python versions
    Document all supported Python versions through trove classifiers. Helps
    library user know, at a glance, if the package is compatible with an
    environment.

--- setup.py
+++ setup.py
@@ -18,4 +18,22 @@ setup(
         'combinators',
     license='MIT',
     url='http://code.google.com/p/funcparserlib/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers'
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     **extra)

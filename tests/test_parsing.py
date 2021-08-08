d4ba3955ffc10544dbae6aaed68bcab21d0c294b
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-08-09 00:31:11 +0300

    Fixed tests compatibility with Python 2.7

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -51,17 +51,14 @@ class ParsingTest(unittest.TestCase):
                 ("nl", (r"[\n\r]+",)),
             ]
         )
-        try:
+        with self.assertRaises(LexerError) as ctx:
             list(tokenize("f is ф"))
-        except LexerError as e:
-            self.assertEqual(
-                six.text_type(e), 'cannot tokenize data: 1,6: "f is \u0444"'
-            )
-        else:
-            self.fail("must raise LexerError")
+        self.assertEqual(
+            six.text_type(ctx.exception), 'cannot tokenize data: 1,6: "f is \u0444"'
+        )
 
         def make_equality(values):
-            # type: (Tuple[str, str]) -> Tuple[str, str]
+            # type: (Tuple[Text, Text]) -> Tuple[Text, Text]
             v1, v2 = values
             return v1, v2
 
@@ -75,21 +72,20 @@ spam is eggs
 foo is_not bar
 end"""
         tokens = [x for x in tokenize(msg) if x.type != "space"]
-        try:
+        with self.assertRaises(NoParseError) as ctx2:
             file.parse(tokens)
-        except NoParseError as e:
-            self.assertEqual(e.state.pos, 4)
-            self.assertEqual(e.state.max, 5)
-            # May raise KeyError
-            t = tokens[e.state.max]
-            self.assertEqual(t, Token("id", "is_not"))
-            self.assertEqual((t.start, t.end), ((2, 5), (2, 10)))
-            self.assertEqual(
-                "2,5-2,10: got unexpected token: 'is_not', expected: 'is'",
-                e.msg,
-            )
+        self.assertEqual(ctx2.exception.state.pos, 4)
+        self.assertEqual(ctx2.exception.state.max, 5)
+        # May raise KeyError
+        t = tokens[ctx2.exception.state.max]
+        self.assertEqual(t, Token("id", "is_not"))
+        self.assertEqual((t.start, t.end), ((2, 5), (2, 10)))
+        if six.PY2:
+            # noinspection SpellCheckingInspection
+            msg = "2,5-2,10: got unexpected token: u'is_not', expected: u'is'"
         else:
-            self.fail("must raise NoParseError")
+            msg = "2,5-2,10: got unexpected token: 'is_not', expected: 'is'"
+        self.assertEqual(ctx2.exception.msg, msg)
 
     def test_ok_ignored(self):
         # type: () -> None
@@ -202,60 +198,66 @@ end"""
         expr = a("x") + a("y")
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xz")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'z', expected: 'y'",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'z', expected: u'y'"
+        else:
+            msg = "got unexpected token: 'z', expected: 'y'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_alt_2_parse_error(self):
         # type: () -> None
         expr = a("x") + (a("x") | a("y"))
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xz")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'z', expected: 'x' or 'y'",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'z', expected: u'x' or u'y'"
+        else:
+            msg = "got unexpected token: 'z', expected: 'x' or 'y'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_alt_3_parse_error(self):
         # type: () -> None
         expr = a("x") + (a("x") | a("y") | a("z"))
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xa")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'a', expected: 'x' or 'y' or 'z'",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'a', expected: u'x' or u'y' or u'z'"
+        else:
+            msg = "got unexpected token: 'a', expected: 'x' or 'y' or 'z'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_alt_3_two_steps_parse_error(self):
         # type: () -> None
         expr = a("x") + (a("x") | (a("y") + a("a")))
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xyz")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'z', expected: 'a'",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'z', expected: u'a'"
+        else:
+            msg = "got unexpected token: 'z', expected: 'a'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_expected_eof_error(self):
         # type: () -> None
         expr = a("x") + finished
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xy")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'y', expected: end of file",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'y', expected: end of file"
+        else:
+            msg = "got unexpected token: 'y', expected: end of file"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_expected_second_in_sequence_error(self):
         # type: () -> None
         expr = a("x") + a("y")
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xz")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'z', expected: 'y'",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'z', expected: u'y'"
+        else:
+            msg = "got unexpected token: 'z', expected: 'y'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_forward_decl_nested_matching_error(self):
         # type: () -> None
@@ -263,27 +265,30 @@ end"""
         expr.define(a("x") + maybe(expr) + a("y"))
         with self.assertRaises(NoParseError) as ctx:
             expr.parse("xxy")
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected end of file, expected: 'y'",
-        )
+        if six.PY2:
+            msg = "got unexpected end of file, expected: u'y'"
+        else:
+            msg = "got unexpected end of file, expected: 'y'"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_expected_token_type_error(self):
         # type: () -> None
         expr = tok("number")
         with self.assertRaises(NoParseError) as ctx:
-            expr.parse([Token("id", "foo")])
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: 'foo', expected: number",
-        )
+            expr.parse([Token("id", "x")])
+        if six.PY2:
+            msg = "got unexpected token: u'x', expected: number"
+        else:
+            msg = "got unexpected token: 'x', expected: number"
+        self.assertEqual(ctx.exception.msg, msg)
 
     def test_expected_exact_token_error(self):
         # type: () -> None
         expr = tok("operator", "=")
         with self.assertRaises(NoParseError) as ctx:
             expr.parse([Token("operator", "+")])
-        self.assertEqual(
-            ctx.exception.msg,
-            "got unexpected token: '+', expected: '='",
-        )
+        if six.PY2:
+            msg = "got unexpected token: u'+', expected: u'='"
+        else:
+            msg = "got unexpected token: '+', expected: '='"
+        self.assertEqual(ctx.exception.msg, msg)
